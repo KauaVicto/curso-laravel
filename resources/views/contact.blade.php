a2814e58f74eab926aaa7f65a53e014ce346f28c
RebbD <kauavictor259@gmail.com>
2021-10-04 16:36:08 -0300

    formulario create

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,4 +7,11 @@
 <h1>Contatos</h1>
 <a href="/">Home</a>
 
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Buscar">
+    </form>
+</div>
+
 @endsection
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,31 @@
 
 @section('content')
 
-<h1>Crie um evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie um Evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Evento">
+        </div>
+        <div class="form-group">
+            <label for="city">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do Evento">
+        </div>
+        <div class="form-group">
+            <label for="private">O evento é privado?</label>
+            <select name="private" id="private" class="form-control">
+                <option value="0" selected>Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="description">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="Sobre o que é o evento?"></textarea>
+        </div>
+        <input type="submit" value="Criar Evento" class="btn btn-primary">
+    </form>
+</div>
 
 @endsection
